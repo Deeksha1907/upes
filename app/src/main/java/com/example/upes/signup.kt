6ce2e29da52d01317ffea12936909ee017bbf573
Deeksha Gupta <deekgupta2004@gmail.com>
2024-03-05 23:42:03 +0530

    signup

---
+++ app/src/main/java/com/example/upes/signup.kt
@@ -0,0 +1,15 @@
+package com.example.upes
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+    }
+}
