07fbdac5cd55df3b4af0d0ef28bc675e4a895abb
itssinghankit <singhankit.kr@gmail.com>
2024-03-08 18:17:28 +0530

    project initialisation

--- app/src/main/java/com/example/upes/presentation/screens/authentication/login/Login.kt
+++ app/src/main/java/com/example/upes/presentation/screens/authentication/login/Login.kt
@@ -1,13 +1,9 @@
-package com.example.upes
-import android.content.Intent
+package com.example.upes.presentation.screens.authentication.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import com.example.upes.R
 
-class login : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
--- app/src/main/java/com/example/upes/presentation/screens/authentication/signup/Signup.kt
+++ app/src/main/java/com/example/upes/presentation/screens/authentication/signup/Signup.kt
@@ -1,11 +1,7 @@
-package com.example.upes
-import android.content.Intent
+package com.example.upes.presentation.screens.authentication.signup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import com.example.upes.R
 
 class signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
