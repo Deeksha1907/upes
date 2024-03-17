ab4a29e57e89da4c187faf822c3e35495464f9fc
Deeksha Gupta <deekgupta2004@gmail.com>
2024-03-17 15:53:17 +0530

    module3

---
+++ app/src/main/java/com/example/upes/presentation/screens/authentication/signup/Pmod3_lvl3_5.kt
@@ -0,0 +1,17 @@
+package com.example.upes.presentation.screens.authentication
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.upes.R
+
+class Pmod3_lvl3_5 : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pmod3_lvl3_5)
+
+
+
+    }
+}
